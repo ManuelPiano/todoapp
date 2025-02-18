9e381a38e7faba4384cd4091b3189cee95859639
Manuel Alvarado <manuel.alvarado21@itca.edu.sv>
2025-02-18 10:45:37 -0600

    Clase 14

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Task} from '../../models/task.model';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+tasks = signal<Task[]>([
+  {
+    id: Date.now(),
+    title: "Crear Proyecto",
+    completed: false
+  },
+  {
+    id: Date.now(),
+    title: "Crear Componentes",
+    completed: false
+  },
+  {
+    id: Date.now(),
+    title: "Crear Servicios",
+    completed: false
+  },
+  ]);
 
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement; 
+    const newTask = input.value;
+    this.addTask(newTask);
+  }
+
+  addTask(title: string) {
+    const newTask: Task = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.tasks.update(tasks => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update(tasks => tasks.filter((_, i) => i !== index));
+  }
+
+  updateTask(index: number) {
+    
+    this.tasks.update(tasks => tasks.map((task, i) => {
+      if (i === index) {
+        return {
+          ...task,
+          completed: !task.completed
+        };
+      }
+      return task;
+    }));
+
+    }
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -9,21 +9,21 @@ import { CommonModule } from '@angular/common';
 })
 export class LabsComponent {
   welcome = 'Hola!';
-  tasks = [
+  tasks = signal([
     'Instalar Angular CLI',
     'Crear nuevo proyecto',
     'Crear componentes',  
-  ];
+  ]);
   name = signal('Manuel');
   age = 34;
   disabled = true;
   img = 'https://angular.io/assets/images/logos/angular/angular.png';
 
-  person = {
+  person = signal({
     name: 'Manuel',
     age: 34,
     avatar: 'https://angular.io/assets/images/logos/angular/angular.png'
-  }
+  })
 
   clickHandler() {
     alert('Hola, ' + this.person.name);
@@ -35,8 +35,8 @@ export class LabsComponent {
     this.name.set(newValue);
   }
 
-  keyHandler(event: KeyboardEvent) {
+  keyHandler(event: Event) {
     const input = event.target as HTMLInputElement; 
-    console.log(input.value);
+    this.name.set(input.value);
   }
 }
