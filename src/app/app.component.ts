0fea1ef60e847adf9d9d70cf9603b0aef2b76de8
Manuel Alvarado <manuel.alvarado21@itca.edu.sv>
2025-03-23 19:32:24 -0600

    mejoras testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8_YkAlGGB6FADO9RuxpomSyBkgHWz8gc",
+  authDomain: "task-app-747eb.firebaseapp.com",
+  projectId: "task-app-747eb",
+  storageBucket: "task-app-747eb.firebasestorage.app",
+  messagingSenderId: "829872554499",
+  appId: "1:829872554499:web:719231d144080fe7c44436",
+  measurementId: "G-1Q1W1TGPLJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule],
